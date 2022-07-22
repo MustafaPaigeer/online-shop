3d88eff65d62d331added400b6ec6877551d16ae
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-21 23:09:22 -0700

    create react component files

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import React from 'react'
   return (
     <>
       HeroBanner
-      <div>
+      <div className="products-heading">
         <h2>Best Selling products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>
+      <div className="products-container">
         {['product 1', 'product 2'].map(
           (product) => product)}
       </div>
