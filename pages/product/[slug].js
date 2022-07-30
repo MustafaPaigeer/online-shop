51a281411a06ccbbf2a213dca068b9aaeb1ea7d2
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-29 21:37:51 -0700

    Add onMouse enter select product image

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { Product } from '../../components';
 import { client, urlFor } from '../../lib/client';
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
 
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} />
+            <img src={urlFor(image && image[index])} className="product-detail-image" />
           </div>
-          {/* <div className="small-images-container">
+          <div className="small-images-container">
             {image?.map((item, i) => (
             <img 
             src={urlFor(item)}
-            className=""
-            onMouseEnter=""
+            className={i === index ? 'small-image selected-image': 'small-image'}
+            onMouseEnter={()=> setIndex(i)}
               /> 
               ))}
-          </div> */}
+          </div>
         </div>
         <div className="product-detail-desc">
           <h1>{name}</h1>
