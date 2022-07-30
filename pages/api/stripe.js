18c72742a8e8a9b3e34714bc90e8824ec177b1f1
MustafaPaigeer <mustafapaigeer@live.com>
2022-07-30 13:13:36 -0700

    Create stripe checkout handler

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -14,15 +14,26 @@ export default async function handler(req, res) {
           { shipping_rate: 'shr_1LRLkmHKAhUQZwvqSvg4n9ny' },
           { shipping_rate: 'shr_1LRLlcHKAhUQZwvqCrCS82wH' },
         ],
-
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: '{{PRICE_ID}}',
-            quantity: 1,
-          },
-        ],
-        mode: 'payment',
+        
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/1tunb3mo/production/').replace('-webp', '.webp');
+          return {
+            price_data: { 
+              currency: 'usd',
+              product_data: { 
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled:true,
+              minimum: 1,
+            },
+            quantity: item.quantity
+          }
+        }),
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
       }
@@ -30,7 +41,7 @@ export default async function handler(req, res) {
       
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+      res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
