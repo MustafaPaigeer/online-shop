8291b87baa5a861f215600484468ce004e6246be
MustafaPaigeer <mustafapaigeer@live.com>
2022-08-01 10:53:47 -0700

    add description and link to shopping to success page

--- pages/success.js
+++ pages/success.js
@@ -15,6 +15,16 @@ const Success = () => {
         <p className="icon">
           <BsBagCheckFill />
         </p>
+        <h2>Thank you for our order!</h2>
+        <p className='email-msg'>Check your email inbox for the receipt.</p>
+        <p className='description'>If you have any question please email
+        <a className='email' href="mailto:order@example.com">order@example.com </a>
+        </p>
+          <Link href="/">
+            <button type='button' width='300px' className='btn'>
+              Continue Shopping
+            </button>
+          </Link>
       </div>
       success
     </div>
