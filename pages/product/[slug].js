2a21a5c39d700606ed2d8602019eef40dad4615c
MustafaPaigeer <mustafapaigeer@live.com>
2022-08-01 23:56:33 -0700

    cleaning the code

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,7 +8,7 @@ const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
   const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
-  
+
   const handleBuyNow = () => {
     onAdd(product, qty);
 
@@ -25,13 +25,13 @@ const ProductDetails = ({ product, products }) => {
           </div>
           <div className="small-images-container">
             {image?.map((item, i) => (
-            <img 
-            key={i}
-            src={urlFor(item)}
-            className={i === index ? 'small-image selected-image': 'small-image'}
-            onMouseEnter={()=> setIndex(i)}
-              /> 
-              ))}
+              <img
+                key={i}
+                src={urlFor(item)}
+                className={i === index ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))}
           </div>
         </div>
         <div className="product-detail-desc">
@@ -61,21 +61,21 @@ const ProductDetails = ({ product, products }) => {
             </p>
           </div>
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick={()=> onAdd(product, qty)}>Add to Cart</button>
+            <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
             <button type="button" className="buy-now" onClick={handleBuyNow}>Buy Now</button>
           </div>
         </div>
       </div>
       <div className="maylike-products-wrapper">
-          <h2>You may also like</h2>
-          <div className="marquee">
-            <div className="maylike-products-container track">
-              {products.map((item) => (
-                <Product key={item._id}
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id}
                 product={item} />
-              ))}
-            </div>
+            ))}
           </div>
+        </div>
       </div>
     </div>
   )
--- pages/success.js
+++ pages/success.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { BsBagCheckFill } from 'react-icons/bs';
 import { useRouter } from 'next/router';
@@ -26,13 +26,13 @@ const Success = () => {
         <h2>Thank you for our order!</h2>
         <p className='email-msg'>Check your email inbox for the receipt.</p>
         <p className='description'>If you have any question please email
-        <a className='email' href="mailto:order@example.com">order@example.com </a>
+          <a className='email' href="mailto:order@example.com">order@example.com </a>
         </p>
-          <Link href="/">
-            <button type='button' width='300px' className='btn'>
-              Continue Shopping
-            </button>
-          </Link>
+        <Link href="/">
+          <button type='button' width='300px' className='btn'>
+            Continue Shopping
+          </button>
+        </Link>
       </div>
       success
     </div>
